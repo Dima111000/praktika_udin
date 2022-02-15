28af9bb65f2fa927bc380bf3bbcab919209a38d0
Dima111000 <74449630+Dima111000@users.noreply.github.com>
2022-02-15 17:15:35 +0200

    Delete js directory

--- js/mask.js
+++
@@ -1,3 +0,0 @@
-$(document).ready(function() {
-    $('.phone-mask__input').mask('+38(099) 99-99-999');
-  });
--- js/preview.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function(){
-    $('.small').click(function(){
-    var image = $(this).attr('src');
-    $('.large-photo__lrg').attr('src',image);
-    });
-    });
